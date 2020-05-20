ca6b6efc81a5f3cd93b07c3ebfb9e0d246193eff
Jose <josecarlosjc30@gmail.com>
2020-05-20 18:39:53 +0200

    Cambio en Factura.java

--- Factura.java
+++ Factura.java
@@ -1,8 +1,11 @@
-public class Factura{
+public class Factura implements IFactura{
 
 	public int num;
 	public float base;
 	public float tipoIva;
-	//public Estados estado;
+	public Estados estado;
 
+	public void cambiarEstado(Estados estado){}
+	public float getTotal(){}
+	public float getCantidadIva(){}
 }
